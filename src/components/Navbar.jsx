67c72e593b33d3cc62b028718b26b0a207975a09
amberlislaya <laya.playapps@gmail.com>
2023-07-23 17:22:46 -0300

    portfallprojects

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { links } from '../data';
+import './navbar.css'
+
+const Navbar = () => {
+	
+  return (
+<nav className="nav">
+<div className="nav__menu">
+<ul className="nav__list">
+      {links.map(({name, icon, path}, index) => {
+   return (
+<li className="nav__item" key={index}>
+ <NavLink to={path} className={({isActive}) => isActive ? 
+ 'nav__link active-nav' : 'nav__link'}>
+      {icon}
+<h3 className='nav__name'>{name}</h3>
+ </NavLink>
+</li>
+);
+})}
+</ul>
+</div>
+<div className="nav__toggle">
+<span></span>
+<span></span>
+<span></span>
+</div>
+</nav>
+  )
+}
+
+export default Navbar
