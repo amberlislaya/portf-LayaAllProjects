597a3eb08dc88a525342516f935f5fe7325eae43
amberlislaya <laya.playapps@gmail.com>
2023-07-28 03:02:49 -0300

    portfoliopart1

--- src/data.jsx
+++ src/data.jsx
@@ -1,5 +1,3 @@
-
-
 import {
     FaHome,
     FaUser,
@@ -11,12 +9,12 @@ import {
 } from 'react-icons/fa';
 import { FiFileText, FiUser, FiExternalLink } from 'react-icons/fi';
 
-// import Work1 from './assets/project-1.jpeg';
-// import Work2 from './assets/project-2.jpg';
-// import Work3 from './assets/project-3.jpeg';
-// import Work4 from './assets/project-4.jpeg';
-// import Work5 from './assets/project-5.jpg';
-// import Work6 from './assets/project-6.jpg';
+import Work1 from './assets/project-1.jpeg';
+import Work2 from './assets/port2.png';
+import Work3 from './assets/port3.png';
+import Work4 from './assets/port4.png';
+import Work5 from './assets/rym.png';
+import Work6 from './assets/rym2.png';
 
 // import Theme1 from './assets/purple.png';
 // import Theme2 from './assets/red.png';
@@ -63,25 +61,25 @@ export const personalInfo = [
     {
         id: 1,
         title: 'First Name : ',
-        description: 'Amberlis',
+        description: 'Steve',
     },
 
     {
         id: 2,
         title: 'Last Name : ',
-        description: 'Laya Madera',
+        description: 'Milner',
     },
 
     {
         id: 3,
         title: 'Age : ',
-        description: '35 Years',
+        description: '27 Years',
     },
 
     {
         id: 4,
         title: 'Nationality : ',
-        description: 'Venezolana',
+        description: 'Tunisian',
     },
 
     {
@@ -93,31 +91,31 @@ export const personalInfo = [
     {
         id: 6,
         title: 'Address : ',
-        description: 'Argentina',
+        description: 'Tunis',
     },
 
     {
         id: 7,
         title: 'Phone : ',
-        description: '+54 9 1122781197',
+        description: '+21621184010',
     },
 
     {
         id: 8,
         title: 'Email : ',
-        description: 'laya.playapps@gmail.com',
+        description: 'you@mail.com',
     },
 
     {
         id: 9,
-        title: 'LinkedIn : ',
-        description: 'Amberlis L M',
+        title: 'Skype : ',
+        description: 'steve.milner',
     },
 
     {
         id: 10,
         title: 'Langages : ',
-        description: 'Spanish, English',
+        description: 'French, English',
     },
 ];
 
@@ -207,7 +205,7 @@ export const skills = [
     {
         id: 1,
         title: 'Html',
-        percentage: '45',
+        percentage: '25',
     },
 
     {
@@ -219,7 +217,13 @@ export const skills = [
     {
         id: 3,
         title: 'Css',
-        percentage: '80',
+        percentage: '70',
+    },
+
+    {
+        id: 4,
+        title: 'Php',
+        percentage: '66',
     },
 
     {
@@ -235,177 +239,183 @@ export const skills = [
     },
 
     {
+        id: 7,
+        title: 'Angular',
+        percentage: '65',
+    },
+
+    {
         id: 8,
         title: 'React',
-        percentage: '80',
+        percentage: '45',
     },
 ];
 
-// export const portfolio = [
-//     {
-//         id: 1,
-//         img: Work1,
-//         title: 'Photo Editing',
-//         details: [
-//             {
-//                 icon: <FiFileText />,
-//                 title: 'Project : ',
-//                 desc: 'Photo',
-//             },
-//             {
-//                 icon: <FiUser />,
-//                 title: 'Client : ',
-//                 desc: 'Dribble',
-//             },
-//             {
-//                 icon: <FaCode />,
-//                 title: 'Language : ',
-//                 desc: 'Adobe Photoshop',
-//             },
-//             {
-//                 icon: <FiExternalLink />,
-//                 title: 'Preview : ',
-//                 desc: 'www.dribble.com',
-//             },
-//         ],
-//     },
+export const portfolio = [
+    {
+        id: 1,
+        img: Work1,
+        title: 'Photo Editing',
+        details: [
+            {
+                icon: <FiFileText />,
+                title: 'Project : ',
+                desc: 'Photo',
+            },
+            {
+                icon: <FiUser />,
+                title: 'Client : ',
+                desc: 'Dribble',
+            },
+            {
+                icon: <FaCode />,
+                title: 'Language : ',
+                desc: 'Adobe Photoshop',
+            },
+            {
+                icon: <FiExternalLink />,
+                title: 'Preview : ',
+                desc: 'www.dribble.com',
+            },
+        ],
+    },
 
-//     {
-//         id: 2,
-//         img: Work2,
-//         title: 'Website Design',
-//         details: [
-//             {
-//                 icon: <FiFileText />,
-//                 title: 'Project : ',
-//                 desc: 'Website',
-//             },
-//             {
-//                 icon: <FiUser />,
-//                 title: 'Client : ',
-//                 desc: 'Dribble',
-//             },
-//             {
-//                 icon: <FaCode />,
-//                 title: 'Language : ',
-//                 desc: 'React JS',
-//             },
-//             {
-//                 icon: <FiExternalLink />,
-//                 title: 'Preview : ',
-//                 desc: 'www.dribble.com',
-//             },
-//         ],
-//     },
+    {
+        id: 2,
+        img: Work2,
+        title: 'Website Design',
+        details: [
+            {
+                icon: <FiFileText />,
+                title: 'Project : ',
+                desc: 'Website',
+            },
+            {
+                icon: <FiUser />,
+                title: 'Client : ',
+                desc: 'Dribble',
+            },
+            {
+                icon: <FaCode />,
+                title: 'Language : ',
+                desc: 'React JS',
+            },
+            {
+                icon: <FiExternalLink />,
+                title: 'Preview : ',
+                desc: 'www.dribble.com',
+            },
+        ],
+    },
 
-//     {
-//         id: 3,
-//         img: Work3,
-//         title: 'Video Editing',
-//         details: [
-//             {
-//                 icon: <FiFileText />,
-//                 title: 'Project : ',
-//                 desc: 'Video',
-//             },
-//             {
-//                 icon: <FiUser />,
-//                 title: 'Client : ',
-//                 desc: 'Dribble',
-//             },
-//             {
-//                 icon: <FaCode />,
-//                 title: 'Language : ',
-//                 desc: 'Adobe Premium',
-//             },
-//             {
-//                 icon: <FiExternalLink />,
-//                 title: 'Preview : ',
-//                 desc: 'www.dribble.com',
-//             },
-//         ],
-//     },
+    {
+        id: 3,
+        img: Work3,
+        title: 'Video Editing',
+        details: [
+            {
+                icon: <FiFileText />,
+                title: 'Project : ',
+                desc: 'Video',
+            },
+            {
+                icon: <FiUser />,
+                title: 'Client : ',
+                desc: 'Dribble',
+            },
+            {
+                icon: <FaCode />,
+                title: 'Language : ',
+                desc: 'Adobe Premium',
+            },
+            {
+                icon: <FiExternalLink />,
+                title: 'Preview : ',
+                desc: 'www.dribble.com',
+            },
+        ],
+    },
 
-//     {
-//         id: 4,
-//         img: Work4,
-//         title: 'Video Editing',
-//         details: [
-//             {
-//                 icon: <FiFileText />,
-//                 title: 'Project : ',
-//                 desc: 'Video',
-//             },
-//             {
-//                 icon: <FiUser />,
-//                 title: 'Client : ',
-//                 desc: 'Dribble',
-//             },
-//             {
-//                 icon: <FaCode />,
-//                 title: 'Language : ',
-//                 desc: 'Adobe Premium',
-//             },
-//             {
-//                 icon: <FiExternalLink />,
-//                 title: 'Preview : ',
-//                 desc: 'www.dribble.com',
-//             },
-//         ],
-//     },
+    {
+        id: 4,
+        img: Work4,
+        title: 'Video Editing',
+        details: [
+            {
+                icon: <FiFileText />,
+                title: 'Project : ',
+                desc: 'Video',
+            },
+            {
+                icon: <FiUser />,
+                title: 'Client : ',
+                desc: 'Dribble',
+            },
+            {
+                icon: <FaCode />,
+                title: 'Language : ',
+                desc: 'Adobe Premium',
+            },
+            {
+                icon: <FiExternalLink />,
+                title: 'Preview : ',
+                desc: 'www.dribble.com',
+            },
+        ],
+    },
 
-//     {
-//         id: 5,
-//         img: Work5,
-//         title: 'Landing Page',
-//         details: [
-//             {
-//                 title: 'Project : ',
-//                 desc: 'Website',
-//             },
-//             {
-//                 title: 'Client : ',
-//                 desc: 'Dribble',
-//             },
-//             {
-//                 title: 'Language : ',
-//                 desc: 'React JS, Node JS',
-//             },
-//             {
-//                 title: 'Preview : ',
-//                 desc: 'www.dribble.com',
-//             },
-//         ],
-//     },
+    {
+        id: 5,
+        img: Work5,
+        title: 'Landing Page',
+        details: [
+            {
+                title: 'Project : ',
+                desc: 'Website',
+            },
+            {
+                title: 'Client : ',
+                desc: 'Dribble',
+            },
+            {
+                title: 'Language : ',
+                desc: 'React JS, Node JS',
+            },
+            {
+                title: 'Preview : ',
+                desc: 'www.dribble.com',
+            },
+        ],
+    },
 
-//     {
-//         id: 6,
-//         img: Work6,
-//         title: 'Photo Editing',
-//         details: [
-//             {
-//                 icon: <FiFileText />,
-//                 title: 'Project : ',
-//                 desc: 'Photo',
-//             },
-//             {
-//                 icon: <FiUser />,
-//                 title: 'Client : ',
-//                 desc: 'Dribble',
-//             },
-//             {
-//                 icon: <FaCode />,
-//                 title: 'Language : ',
-//                 desc: 'Adobe Photoshop',
-//             },
-//             {
-//                 icon: <FiExternalLink />,
-//                 title: 'Preview : ',
-//                 desc: 'www.dibble.com',
-//             },
-//         ],
-//     },
-// ];
+    {
+        id: 6,
+        img: Work6,
+        title: 'Photo Editing',
+        details: [
+            {
+                icon: <FiFileText />,
+                title: 'Project : ',
+                desc: 'Photo',
+            },
+            {
+                icon: <FiUser />,
+                title: 'Client : ',
+                desc: 'Dribble',
+            },
+            {
+                icon: <FaCode />,
+                title: 'Language : ',
+                desc: 'Adobe Photoshop',
+            },
+            {
+                icon: <FiExternalLink />,
+                title: 'Preview : ',
+                desc: 'www.dibble.com',
+            },
+        ],
+    },
+];
 
 // export const themes = [
 //     {
