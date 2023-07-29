f32009c416040f8646ba164a34a13e2cae64756a
amberlislaya <laya.playapps@gmail.com>
2023-07-29 17:18:36 -0300

    modi

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
  <FaEnvelopeOpen className='info__icon'/>
 
 <div>
-<span className="info__title">Email me</span>
+<span className="info__title">Mail me</span>
 <h4 className="info__desc">laya.playapps@gmail.com</h4>
 </div>
 </div>
