d5902e9ea0e7b6398fa67f3992f49c8d9f63d6c0
amberlislaya <laya.playapps@gmail.com>
2024-02-18 03:42:21 -0300

    langos

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
-import Navbar from './components/Navbar'
-import Home from './pages/home/Home'
-import Themes from './components/Themes'
-import About from './pages/about/About'
-import Portfolio from './pages/portfolio/Portfolio'
-import Contact from './pages/contact/Contact'
-
+import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/home/Home';
+import Themes from './components/Themes';
+import About from './pages/about/About';
+import Portfolio from './pages/portfolio/Portfolio';
+import Contact from './pages/contact/Contact';
 
 function App() {
+
   return (
-  <BrowserRouter>
-  <Navbar/>
-  <Themes/>
-   <Routes>
-		<Route index element={<Home/>}/>
-		<Route path='about' element={<About/>}/>
-		<Route path='portfolio' element={<Portfolio/>}/>
-		<Route path='contact' element={<Contact />} />
-	</Routes>
-  </BrowserRouter> 
-)
+<BrowserRouter>
+<Navbar />
+<Themes />
+<Routes>
+<Switch>
+<Route index element={<Home />} />
+<Route path='/about' element={<About />} />
+<Route path='/portfolio' element={<Portfolio />} />
+<Route path='/contact' element={<Contact />} />
+</Switch>
+</Routes>
+</BrowserRouter>
+);
 }
 
 export default App;
