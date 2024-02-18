beff2ef6a656ca0d290846fdc679d5f21a01dbe2
amberlislaya <laya.playapps@gmail.com>
2024-02-18 03:25:27 -0300

    hted

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 		<Route index element={<Home/>}/>
 		<Route path='about' element={<About/>}/>
 		<Route path='portfolio' element={<Portfolio/>}/>
-		<Route path='contact' element={<Contact/>}/>
+		<Route path='/contact' element={<Contact/>}/>
 	</Routes>
   </BrowserRouter> 
 )
