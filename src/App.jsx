7599b366afa35834247b88eea716aecf44eca8e7
amberlislaya <laya.playapps@gmail.com>
2024-02-18 03:35:20 -0300

    ftrevg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './pages/home/Home'
@@ -6,6 +6,7 @@ import Themes from './components/Themes'
 import About from './pages/about/About'
 import Portfolio from './pages/portfolio/Portfolio'
 import Contact from './pages/contact/Contact'
+
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
 		<Route index element={<Home/>}/>
 		<Route path='about' element={<About/>}/>
 		<Route path='portfolio' element={<Portfolio/>}/>
-		<Route path='/contact' element={<Contact/>}/>
+		<Route path='contact' element={<Contact />} />
 	</Routes>
   </BrowserRouter> 
 )
